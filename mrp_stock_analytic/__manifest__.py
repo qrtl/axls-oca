7136cb48b1c60f50edf27a3733fdf9357ce00d59
Rinaldi Firdaus <rinaldi@quartile.co>
2023-02-20 04:41:27 +0000

    [3159][UPD] mrp_stock_analytic, mrp_subcontracting_analytic

--- mrp_stock_analytic/__manifest__.py
+++ mrp_stock_analytic/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2023 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "MRP Production Analytic",
+    "name": "MRP Stock Analytic",
     "version": "16.0.1.0.0",
     "author": "Quartile Limited, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-analytic",
