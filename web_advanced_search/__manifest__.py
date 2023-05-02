63855ec9aef78e6edfd929fd395e837a4452a7b9
Yoshi Tashiro <tashiro@quartile.co>
2023-05-02 09:53:03 +0000

    [3419][UPD] web_advanced_search

--- web_advanced_search/__manifest__.py
+++ web_advanced_search/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Advanced search",
     "summary": "Easier and more powerful searching tools",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "author": "Therp BV, Tecnativa, Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
     "maintainers": ["ivantodorovich"],
@@ -15,8 +15,9 @@
     "depends": ["web"],
     "assets": {
         "web.assets_backend": [
-            "web_advanced_search/static/src/js/**/*.js",
-            "web_advanced_search/static/src/xml/**/*.xml",
+            "web_advanced_search/static/src/js/**/*",
+            "web_advanced_search/static/src/legacy/**/*",
+            "web_advanced_search/static/src/search/**/*",
         ]
     },
 }
