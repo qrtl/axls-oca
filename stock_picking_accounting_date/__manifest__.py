61817ccc7a95317bcd3d394fc42984e430903a69
Rinaldi Firdaus <rinaldi@quartile.co>
2023-05-22 03:23:08 +0000

    [3439][ADD] stock_picking_accounting_date

---
+++ stock_picking_accounting_date/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Picking Accounting Date",
+    "version": "16.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Stock",
+    "license": "AGPL-3",
+    "depends": ["stock_account"],
+    "data": [
+        "views/stock_picking_views.xml",
+    ],
+    "installable": True,
+}
