deff6fc615e1a13a8698d63e941910be13212576
Aungkokolin1997 <lin@quartile.co>
2023-06-16 10:12:31 +0630

    upd

---
+++ analytic_mixin_analytic_account/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2023 Quartile Limited (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Analytic Mixin Analytic Account",
+    "version": "16.0.1.0.0",
+    "author": "Quartile Limited, Odoo Community Association (OCA)",
+    "category": "Analytic Accounting",
+    "website": "https://github.com/OCA/account-analytic",
+    "depends": ["analytic"],
+    "license": "AGPL-3",
+    "installable": True,
+}
