b6d9dd310fba98b6e5942ac536b5b372d104308d
kanda999 <kanda@quartile.co>
2023-09-27 08:16:09 +0000

    update orestriction

--- mrp_stock_owner_restriction/models/mrp_unbuild.py
+++ mrp_stock_owner_restriction/models/mrp_unbuild.py
@@ -19,7 +19,8 @@ class MrpUnbuild(models.Model):
     # owner_restriction = fields.Selection(related="picking_type_id.owner_restriction")
 
     def action_validate(self):
+        owner_restriction = self.mo_id.picking_type_id.owner_restriction
         owner = self.mo_id.owner_id
-        if owner:
+        if owner and owner_restriction != "standard_behavior":
             self = self.with_context(force_restricted_owner_id=owner)
         return super().action_validate()
