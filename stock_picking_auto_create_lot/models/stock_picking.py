9cb730394791e2a35a055b8fa26536ee02ed4f12
Aungkokolin1997 <lin@quartile.co>
2024-02-23 04:56:28 +0000

    [16.0][FIX] stock_picking_auto_create_lot: fix creating lot for 0 qty move lines for serial product

--- stock_picking_auto_create_lot/models/stock_picking.py
+++ stock_picking_auto_create_lot/models/stock_picking.py
@@ -2,6 +2,7 @@
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import models
+from odoo.osv import expression
 
 
 class StockPicking(models.Model):
@@ -11,16 +12,25 @@ class StockPicking(models.Model):
         """
         Allows to be called either by button or through code
         """
-        pickings = self.filtered(lambda p: p.picking_type_id.auto_create_lot)
-        lines = pickings.mapped("move_line_ids").filtered(
-            lambda x: (
-                not x.lot_id
-                and not x.lot_name
-                and x.product_id.tracking != "none"
-                and x.product_id.auto_create_lot
-            )
-        )
-        lines.set_lot_auto()
+        lines = self.env["stock.move.line"]
+        for picking in self:
+            if not picking.picking_type_id.auto_create_lot:
+                continue
+            domain = [
+                ("picking_id", "=", picking.id),
+                ("lot_id", "=", False),
+                ("lot_name", "=", False),
+                ("product_id.tracking", "!=", "none"),
+                ("product_id.auto_create_lot", "=", True),
+            ]
+            # i.e. There is a move line with done quantity in the picking.
+            # This is to prevent unnecessary generation of unused serials in case of
+            # partial receipts of serial-tracked products.
+            if not picking._check_immediate():
+                domain = expression.AND([domain, [("qty_done", ">", 0)]])
+            lines += lines.search(domain)
+        if lines:
+            lines.set_lot_auto()
 
     def _action_done(self):
         self._set_auto_lot()
