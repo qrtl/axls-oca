b9a5c3080f336219c227fe5c6374b34994ea83a0
Yoshi Tashiro <tashiro@quartile.co>
2024-03-19 14:40:31 +0000

    [4410][FIX] stock_picking_accounting_date: consider force_period

--- stock_picking_accounting_date/models/stock_move.py
+++ stock_picking_accounting_date/models/stock_move.py
@@ -1,4 +1,4 @@
-# Copyright 2023 Quartile Limited
+# Copyright 2023-2024 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
@@ -12,12 +12,13 @@ class StockMove(models.Model):
         store=True,
     )
 
-    @api.depends("date", "accounting_date")
+    @api.depends("date", "picking_id.accounting_date")
     def _compute_accounting_date(self):
-        for line in self:
-            line.accounting_date = fields.Datetime.context_timestamp(self, line.date)
-            if line.picking_id.accounting_date:
-                line.accounting_date = line.picking_id.accounting_date
+        for rec in self:
+            if rec.picking_id.accounting_date:
+                rec.accounting_date = rec.picking_id.accounting_date
+                continue
+            rec.accounting_date = fields.Datetime.context_timestamp(self, rec.date)
 
     def _prepare_account_move_vals(
         self,
@@ -38,6 +39,10 @@ class StockMove(models.Model):
             svl_id,
             cost,
         )
+        # i.e. Inventory adjustments with accounting date
+        if self._context.get("force_period_date"):
+            self.write({"accounting_date": self._context["force_period_date"]})
+            return am_vals
         if self.accounting_date:
             am_vals.update({"date": self.accounting_date})
         return am_vals
