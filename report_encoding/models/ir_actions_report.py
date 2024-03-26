79437ab99bdd0bc626d194f3d064db39430c131b
Aungkokolin1997 <lin@quartile.co>
2024-03-26 03:51:35 +0000

    [ADD][REF] report_encoding, report_csv

---
+++ report_encoding/models/ir_actions_report.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Quartile Limited
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class IrActionReport(models.Model):
+    _inherit = "ir.actions.report"
+
+    encoding = fields.Char(
+        help="Encoding to be applied to the generated CSV file. " "e.g. cp932"
+    )
+    encode_error_handling = fields.Selection(
+        selection=[("ignore", "Ignore"), ("replace", "Replace")],
+        help="If nothing is selected, CSV export will fail with an error message when "
+        "there is a character that fail to be encoded.",
+    )
