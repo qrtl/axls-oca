60c9c3d5810dff13b23b6f192b318491a4c2b6e2
Aungkokolin1997 <lin@quartile.co>
2024-04-02 04:33:41 +0000

    fixup

--- report_format_option/models/ir_actions_report.py
+++ report_format_option/models/ir_actions_report.py
@@ -1,7 +1,7 @@
 # Copyright 2024 Quartile Limited
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class IrActionReport(models.Model):
@@ -19,8 +19,10 @@ class IrActionReport(models.Model):
         compute="_compute_show_encoding",
         help="Technical field to control the visibility of the encoding field.",
     )
-    line_ending = fields.Char(
-        help="Line ending to be applied to the generated report. e.g. crlf"
+    line_ending = fields.Selection(
+        [("crlf", "CRLF (\\r\\n)"), ("cr", "CR (\\r)")],
+        help="Select the type of line ending in case the report needs "
+        "to be output with other line ending than 'LF'.",
     )
     show_line_ending = fields.Boolean(
         compute="_compute_show_line_ending",
@@ -31,8 +33,31 @@ class IrActionReport(models.Model):
         """Extend this method to show the encoding field in the report form."""
         for report in self:
             report.show_encoding = False
+            if report.report_type == "qweb-text":
+                report.show_encoding = True
 
     def _compute_show_line_ending(self):
         """Extend this method to show the line ending field in the report form."""
         for report in self:
             report.show_line_ending = False
+            if report.report_type == "qweb-text":
+                report.show_line_ending = True
+
+    @api.model
+    def _render_qweb_text(self, report_ref, docids, data=None):
+        content, content_type = super()._render_qweb_text(report_ref, docids, data)
+        report = self._get_report(report_ref)
+        if not report.encoding and not report.line_ending:
+            return content, content_type
+        content_str = content.decode("utf-8")
+        if report.line_ending == "crlf":
+            content_str = content_str.replace("\n", "\r\n")
+        elif report.line_ending == "cr":
+            content_str = content_str.replace("\n", "\r")
+        # If specific encoding is set on the report, use it; otherwise, fallback to utf-8
+        encoding = report.encoding or "utf-8"
+        encode_options = {}
+        if report.encode_error_handling:
+            encode_options["errors"] = report.encode_error_handling
+        content = content_str.encode(encoding, **encode_options)
+        return content, content_type
--- report_format_option/readme/DESCRIPTION.rst
+++ report_format_option/readme/DESCRIPTION.rst
@@ -1,2 +1,2 @@
-This is a technical module to add encoding fields to ir.actions.report model and it does nothing by itself.
+This is a technical module designed to add encoding fields to the ir.actions.report model and is applied to Text type reports.
 The module is expected to be a dependency of other reporting modules (e.g., report_csv).
