9b8506df45bc24b43756c62f6090a47bd82bb482
Yoshi Tashiro <tashiro@quartile.co>
2024-04-12 10:07:42 +0000

    fixup!

--- procurement_mto_analytic/models/stock_move.py
+++ procurement_mto_analytic/models/stock_move.py
@@ -9,9 +9,6 @@ class StockMove(models.Model):
     def _prepare_procurement_values(self):
         res = super()._prepare_procurement_values()
         analytic_distribution = self.sale_line_id.analytic_distribution
-        # In case of using stock_analytic and mrp_stock_analytic
-        if not analytic_distribution and hasattr(self, "analytic_distribution"):
-            analytic_distribution = self.analytic_distribution
         # Update the procurement values with analytic_distribution if it exists
         if analytic_distribution:
             res.update({"analytic_distribution": analytic_distribution})
