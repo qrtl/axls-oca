47ac38d86dab86d88a76f2128529efa685101b08
Yoshi Tashiro <tashiro@quartile.co>
2024-04-13 08:33:01 +0000

    add docstring

--- procurement_mto_analytic/models/stock_rule.py
+++ procurement_mto_analytic/models/stock_rule.py
@@ -1,3 +1,4 @@
+# Copyright 2024 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models
@@ -7,6 +8,13 @@ class StockRule(models.Model):
     _inherit = "stock.rule"
 
     def _make_po_get_domain(self, company_id, values, partner):
+        """Consider analytic distribution at purchase order level.
+
+        This logic is expected to be moved into another custom module (or made
+        configurable in this module, if that makes sense...) once the migration PR is
+        merged in the OCA repo.
+        See https://github.com/OCA/account-analytic/pull/637#discussion_r1563828810
+        """
         domain = super()._make_po_get_domain(company_id, values, partner)
         if not values.get("analytic_distribution"):
             return domain
@@ -15,13 +23,12 @@ class StockRule(models.Model):
             .sudo()
             .search([dom for dom in domain])
             .order_line.filtered(
-                lambda x: x.analytic_distribution == values.get("analytic_distribution")
+                lambda x: x.analytic_distribution == values["analytic_distribution"]
             )
         )
         if matching_lines:
             domain += (("order_line", "in", tuple(matching_lines.ids)),)
         else:
-            domain += (
-                ("order_line", "=", False),
-            )  # This might need adjustment based on your intent.
+            # i.e. create a new purchase order
+            domain += (("order_line", "=", False),)
         return domain
