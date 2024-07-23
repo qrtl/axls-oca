df8683446efd7b692a9f2aaea63ef1f17ccd01e9
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2024-07-23 15:28:13 +0700

    [4337][FIX] stock_picking_auto_create_lot: fix creating lot for 0 qty move lines for serial product (#109)
    * [16.0][FIX] stock_picking_auto_create_lot: fix creating lot for 0 qty move lines for serial product

--- stock_picking_auto_create_lot/models/stock_move_line.py
+++ stock_picking_auto_create_lot/models/stock_move_line.py
@@ -1,39 +1,12 @@
-# Copyright 2020 ACSONE SA/NV
+# Copyright 2024 Quartile Limited
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 from odoo import models
-from odoo.fields import first
 
 
 class StockMoveLine(models.Model):
-
     _inherit = "stock.move.line"
 
-    def _get_value_production_lot(self):
-        res = super()._get_value_production_lot()
-        if "name" in res and not res["name"]:
-            del res["name"]
-        return res
-
-    def set_lot_auto(self):
-        """
-        Create lots using create_multi to avoid too much queries
-        As move lines were created by product or by tracked 'serial'
-        products, we apply the lot with both different approaches.
-        """
-        values = []
-        stock_lot_obj = self.env["stock.lot"]
-        lots_by_product = dict()
-        for line in self:
-            # Prepare multi valued lots per line to use multi creation.
-            values.append(line._get_value_production_lot())
-        lots = stock_lot_obj.create(values)
-        for lot in lots:
-            if lot.product_id.id not in lots_by_product:
-                lots_by_product[lot.product_id.id] = lot
-            else:
-                lots_by_product[lot.product_id.id] += lot
-        for line in self:
-            lot = first(lots_by_product[line.product_id.id])
-            line.lot_id = lot
-            if lot.product_id.tracking == "serial":
-                lots_by_product[line.product_id.id] -= lot
+    def _get_lot_sequence(self):
+        self.ensure_one()
+        return self.env["ir.sequence"].next_by_code("stock.lot.serial")
