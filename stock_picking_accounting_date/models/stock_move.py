061687f22c9be2a141c9de3ca84230bfe4c8b7f6
Aungkokolin1997 <lin@quartile.co>
2024-08-16 02:06:59 +0000

    adj

--- stock_picking_accounting_date/models/stock_move.py
+++ stock_picking_accounting_date/models/stock_move.py
@@ -49,7 +49,8 @@ class StockMove(models.Model):
 
     def _get_price_unit(self):
         """Passes the accounting_date to be used in currency conversion for receipts
-        in foreign currency purchases."""
+        in foreign currency purchases.
+        """
         self.ensure_one()
         self = self.with_context(accounting_date=self.accounting_date)
         return super()._get_price_unit()
