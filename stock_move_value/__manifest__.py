704bd43d23a3c409477458622965043a265e4845
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2024-11-25 14:10:58 +0700

    [4685][ADD] stock_move_value (#147)
    [4685][ADD] stock_move_value (#147)

---
+++ stock_move_value/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 Quartile (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Move Value",
+    "version": "16.0.1.0.0",
+    "author": "Quartile, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "category": "stock",
+    "license": "AGPL-3",
+    "depends": ["stock_account"],
+    "data": [
+        "views/stock_move_views.xml",
+    ],
+    "installable": True,
+}
