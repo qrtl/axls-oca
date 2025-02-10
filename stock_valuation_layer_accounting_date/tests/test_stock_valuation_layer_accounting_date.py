76ab7d3ee264ff2b8d4b5ce63ab2d1f0604fa062
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2025-02-10 14:25:07 +0630

    [5096][FIX] stock_valuation_layer_accounting_date: update tests (#174)
    [5096][FIX] stock_valuation_layer_accounting_date: update tests (#174)

--- stock_valuation_layer_accounting_date/tests/test_stock_valuation_layer_accounting_date.py
+++ stock_valuation_layer_accounting_date/tests/test_stock_valuation_layer_accounting_date.py
@@ -5,6 +5,7 @@ from datetime import date
 
 from freezegun import freeze_time
 
+from odoo import fields
 from odoo.tests import tagged
 
 from odoo.addons.stock_account.tests.test_stockvaluationlayer import (
@@ -34,9 +35,10 @@ class TestStockValuationStandard(TestStockValuationStandard):
     def test_svl_accounting_date_manual_periodic(self):
         # Not using freeze_time() in this test since it cannot be applied to create_date
         # without a hack.
-        self.product1.product_tmpl_id.categ_id.property_valuation = "manual_periodic"
+        self.product1.categ_id.property_valuation = "manual_periodic"
         self._make_in_move(self.product1, 10)
         valuation_layer = self.product1.stock_valuation_layer_ids
         self.assertEqual(
-            valuation_layer.accounting_date, valuation_layer.create_date.date()
+            valuation_layer.accounting_date,
+            fields.Date.context_today(valuation_layer, valuation_layer.create_date),
         )
