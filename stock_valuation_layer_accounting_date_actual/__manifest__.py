d23b256610bdd807ff7da6fde946d7c6d0066161
Aungkokolin1997 <lin@quartile.co>
2025-03-19 04:37:15 +0000

    rename module name

--- stock_valuation_layer_accounting_date_actual/__manifest__.py
+++ stock_valuation_layer_accounting_date_actual/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2025 Quartile (https://www.quartile.co)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Stock Valuation Layer Actual Accounting Date",
+    "name": "Stock Valuation Layer Accounting Date Actual",
     "version": "16.0.1.0.0",
     "author": "Quartile, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
--- stock_valuation_layer_actual_accounting_date/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import test_stock_valuation_layer_actual_accounting_date
