f7ca771e971f2fdf971608bf42a37c8bea369032
kanda999 <kanda@quartile.co>
2025-04-01 09:57:30 +0000

    [5276][FIX] stock_move_value

--- stock_move_value/models/stock_move.py
+++ stock_move_value/models/stock_move.py
@@ -55,8 +55,8 @@ class StockMove(models.Model):
                 lambda r: r.quantity > 0
             )
             origin_values[move.id] = {
-                "remaining_qty": origin_svls.remaining_qty,
-                "remaining_value": origin_svls.remaining_value,
+                "quantity": origin_svls.quantity,
+                "unit_cost": origin_svls.unit_cost,
             }
         moves = super()._action_done(cancel_backorder)
         for move in moves:
@@ -64,9 +64,9 @@ class StockMove(models.Model):
             if not move._is_out() or not move.origin_returned_move_id:
                 continue
             move.move_origin_value = (
-                origin_values[move.id]["remaining_value"]
+                origin_values[move.id]["unit_cost"]
                 * move.product_qty
-                / origin_values[move.id]["remaining_qty"]
+                / origin_values[move.id]["quantity"]
             )
             move.value_discrepancy = move.move_origin_value + move.move_value
             if move.value_discrepancy != 0.0:
