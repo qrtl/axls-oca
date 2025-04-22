29b6a10f9fab5ed9da8bd6fd77880187c86db033
Aungkokolin1997 <lin@quartile.co>
2025-04-22 02:40:26 +0000

    [IMP] analytic_mixin_analytic_account: reflect oca changes

--- analytic_mixin_analytic_account/__manifest__.py
+++ analytic_mixin_analytic_account/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright 2023 Quartile Limited (https://www.quartile.co)
+# Copyright 2023 Quartile (https://www.quartile.co)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Analytic Mixin Analytic Account",
     "version": "16.0.1.0.0",
-    "author": "Quartile Limited, Odoo Community Association (OCA)",
+    "author": "Quartile, Odoo Community Association (OCA)",
     "category": "Analytic Accounting",
     "website": "https://github.com/OCA/account-analytic",
     "depends": ["analytic"],
