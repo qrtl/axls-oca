741b317413205bce28dbd2a91d1f422bab2f812c
Aungkokolin1997 <lin@quartile.co>
2025-08-18 05:43:03 +0000

    reflect oca changes

--- account_analytic_relation_filter/static/src/component/analytic_distribution.esm.js
+++ account_analytic_relation_filter/static/src/component/analytic_distribution.esm.js
@@ -10,22 +10,40 @@ patch(AnalyticDistribution.prototype, "custom.analytic_distribution.patch", {
         this.relatedAccountIDs = useState([]);
     },
 
+    async willStart() {
+        await this._super(...arguments);
+        await this._updateRelatedAccountIDs();
+    },
+
     async onSelect(option, params, tag) {
         await this._super(option, params, tag);
-        const selectedIds = this.existingAnalyticAccountIDs;
-        if (selectedIds.length) {
-            const relatedIds = await this.orm.call(
-                "account.analytic.account",
-                "get_related_account_ids",
-                [selectedIds]
-            );
-            this.relatedAccountIDs.splice(0);
-            this.relatedAccountIDs.push(...relatedIds);
-        }
+        await this._updateRelatedAccountIDs();
     },
 
     async deleteTag(id, fromGroup) {
         await this._super(id, fromGroup);
+        await this._updateRelatedAccountIDs();
+    },
+
+    async _onSearchMore(searchTerm, editedTag) {
+        const originalAddDialog = this.addDialog.bind(this);
+        this.addDialog = (component, props, options) => {
+            if (props && typeof props.onSelected === "function") {
+                const originalOnSelected = props.onSelected;
+                props.onSelected = async (...args) => {
+                    const result = await originalOnSelected(...args);
+                    await this._updateRelatedAccountIDs();
+                    return result;
+                };
+            }
+            // Restore immediately after intercepting
+            this.addDialog = originalAddDialog;
+            return originalAddDialog(component, props, options);
+        };
+        return await this._super(searchTerm, editedTag);
+    },
+
+    async _updateRelatedAccountIDs() {
         if (!this.existingAnalyticAccountIDs.length) {
             this.relatedAccountIDs.splice(0);
             return;
