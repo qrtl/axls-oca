886a6dc296e40742c2c75e153d94ce57a7aa0578
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2025-11-25 12:30:41 +0700

    [5799][IMP] product_lot_sequence: reflect oca changes #208
    [5799][IMP] product_lot_sequence: reflect oca changes #208

--- product_lot_sequence/models/stock_move.py
+++ product_lot_sequence/models/stock_move.py
@@ -12,12 +12,8 @@ class StockMove(models.Model):
         if seq_policy in ("product", "global"):
             # If move is not supposed to assign serial pass empty string for next serial
             if not self.display_assign_serial:
-                return super(
-                    StockMove, self.with_context(force_next_serial="")
-                ).action_show_details()
+                self = self.with_context(force_next_serial="")
             # If the sequence was already called once, avoid calling it another time
             elif self.next_serial:
-                return super(
-                    StockMove, self.with_context(force_next_serial=self.next_serial)
-                ).action_show_details()
-        return super().action_show_details()
+                self = self.with_context(force_next_serial=self.next_serial)
+        return super(StockMove, self).action_show_details()
